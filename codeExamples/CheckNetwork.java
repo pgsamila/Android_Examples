8373d086ca81e6ba29e0cfa86d478e854a3a1648
Amila Sampath <pgsamila@yahoo.com>
2016-09-30 09:13:50 +0530

    Update CheckNetwork.java

--- codeExamples/CheckNetwork.java
+++ codeExamples/CheckNetwork.java
@@ -5,3 +5,15 @@
             Intent intent = new Intent(Settings.ACTION_DATA_ROAMING_SETTINGS);
             startActivity(intent);
         }
+
+
+ public boolean isNetworkAvailable() {
+        ConnectivityManager cm = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = cm.getActiveNetworkInfo();
+
+        if (networkInfo != null && networkInfo.isConnected()) {
+            return true;
+        }
+        return false;
+    }
